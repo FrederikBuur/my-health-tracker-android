86a8ca097e6d06f4b6e97b5cac44a7145b87fb9f
Frederik <frederik_buur@hotmail.com>
2021-03-24 17:42:56 +0100

    added room explorer when clicking on statistics page

--- app/src/main/java/com/fbuur/myhealthtracker/pages/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/fbuur/myhealthtracker/pages/statistics/StatisticsFragment.kt
@@ -1,9 +1,37 @@
 package com.fbuur.myhealthtracker.pages.statistics
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.fbuur.myhealthtracker.R
+import com.fbuur.myhealthtracker.data.TrackingDatabase
+import com.fbuur.myhealthtracker.databinding.FragmentEventsBinding
+import com.fbuur.myhealthtracker.databinding.FragmentStatisticsBinding
+import com.wajahatkarim3.roomexplorer.RoomExplorer
 
 class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
 
+    private var _binding: FragmentStatisticsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentStatisticsBinding.inflate(inflater, container, false)
+        setup()
+        return binding.root
+    }
+
+    private fun setup() {
+
+        binding.root.setOnClickListener { v ->
+            RoomExplorer.show(context, TrackingDatabase::class.java, "tracking_database")
+        }
+
+    }
 
 }
